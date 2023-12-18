63e60ca69dcf30c05eb5198aec3415a36a6740e4
Tomasz Zaniewski <tomasz.zaniewski@gmail.com>
2023-12-18 23:01:50 +0200

    it draws

--- DrawMe/draw-me-lego.py
+++ DrawMe/draw-me-lego.py
@@ -13,17 +13,22 @@ class LegoArtPic:
         return self.pilsY * self.pillSize
 
     
-def generatePillArt(legoArtPic, inputPixelMap, outFileName):
+def generatePillArt(legoArtPic, inputImage, outFileName):
+    colorAdjustedImage = inputImage.convert(mode="P", palette=1, colors=16, dither=3).resize([48, 48])
+    inputPixelMap = colorAdjustedImage.load()
+    inputPalette = colorAdjustedImage.getpalette()
+
     image = Image.new("RGB", (legoArtPic.pixelLength(), legoArtPic.pixelHeight()), "#000012")
     draw = ImageDraw.Draw(image)
     for x in range(legoArtPic.pilsX):
         for y in range(legoArtPic.pilsY):
             px = x * legoArtPic.pillSize
             py = y * legoArtPic.pillSize
-            sourceColor = inputPixelMap[x, y]
-            #outlineColor = (sourceColor[0] + 25, sourceColor[1] + 25, sourceColor[2] + 25)
+            paletteIndex = inputPixelMap[x, y] * 3
+            fillColor = (inputPalette[paletteIndex], inputPalette[paletteIndex + 1], inputPalette[paletteIndex +2])
+            outlineColor = (fillColor[0] + 25, fillColor[1] + 25, fillColor[2] + 25)
             points = [(px, py), (px + legoArtPic.pillSize, py + legoArtPic.pillSize)]
-            draw.ellipse(points, fill=sourceColor, width=3)
+            draw.ellipse(points, fill=fillColor, width=3, outline=outlineColor)
     image.save(outFileName)
     image.show()
 
@@ -31,31 +36,16 @@ def generatePillArt(legoArtPic, inputPixelMap, outFileName):
 exp = LegoArtPic(50, 48, 48)
 
 inputImage =  Image.open("Boerneef.jpg")
-colorAdjust = inputImage.convert(mode="P", palette=1, colors=16, dither=3)
+#print('Colors in P mode image: {cols}'.format(cols=len(colorAdjust.getcolors())))
 #colorAdjust = inputImage.convert(mode="RGB", colors=16, dither=3)
-#colorAdjust.save("lowCol4.gif")
-#rgbImage = colorAdjust.convert(mode="RGB", colors=16, palette=1)
-#rgbImage.save("lowColRGB.jpg")
-#inputImage2 = Image.open("lowCol3.gif")
-smallNeef = colorAdjust.resize([48, 48])
-pixelMap = smallNeef.load()
-colors = smallNeef.getcolors()
-palette = smallNeef.getpalette()
-print(palette[13 * 3])
-print(palette[13 * 3 + 1])
-print(palette[13 * 3 + 2])
+#rgbImage = colorAdjust.convert(mode="RGB", colors=16, palette=1).resize([48,48])
+#print('Colors in RGB mode image: {cols}'.format(cols=len(rgbImage.getcolors())))
+
+#generatePillArt(exp, inputImage, "20231218.gif")
 
 
-
-#smallNeef.save("smallneef3.gif")
-#print(pixelMap[0,0])
-#print(pixelMap[1,0])
-#print(pixelMap[2,0])
-#print(pixelMap[3,0])
-#print(pixelMap[4,0])
-#print(pixelMap[5,0])
-#print(pixelMap[6,0]) #13
-#generatePillArt(exp, pixelMap, "20231212.gif")
-
-
+#TODO: print col number in each pill
+#TODO: split into 9 (3 x 3) tiles
+#TODO: load custom palette
+#TODO: source image as parameter
 
