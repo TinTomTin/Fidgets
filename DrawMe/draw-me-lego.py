fa136dd74ac73bfb1395d2d37b14267ce163c6c8
Tomasz Zaniewski <tomasz.zaniewski@gmail.com>
2023-12-18 22:18:27 +0200

    there we were

--- DrawMe/draw-me-lego.py
+++ DrawMe/draw-me-lego.py
@@ -21,22 +21,41 @@ def generatePillArt(legoArtPic, inputPixelMap, outFileName):
             px = x * legoArtPic.pillSize
             py = y * legoArtPic.pillSize
             sourceColor = inputPixelMap[x, y]
-            outlineColor = (sourceColor[0] + 5, sourceColor[1] + 5, sourceColor[2] + 5)
+            #outlineColor = (sourceColor[0] + 25, sourceColor[1] + 25, sourceColor[2] + 25)
             points = [(px, py), (px + legoArtPic.pillSize, py + legoArtPic.pillSize)]
-            draw.ellipse(points, fill=sourceColor, outline="#888888", width=3)
+            draw.ellipse(points, fill=sourceColor, width=3)
     image.save(outFileName)
+    image.show()
 
 
 exp = LegoArtPic(50, 48, 48)
 
 inputImage =  Image.open("Boerneef.jpg")
-inputImage.load()
-
-smallNeef = inputImage.resize([48, 48])
+colorAdjust = inputImage.convert(mode="P", palette=1, colors=16, dither=3)
+#colorAdjust = inputImage.convert(mode="RGB", colors=16, dither=3)
+#colorAdjust.save("lowCol4.gif")
+#rgbImage = colorAdjust.convert(mode="RGB", colors=16, palette=1)
+#rgbImage.save("lowColRGB.jpg")
+#inputImage2 = Image.open("lowCol3.gif")
+smallNeef = colorAdjust.resize([48, 48])
 pixelMap = smallNeef.load()
-#smallNeef.save("smallneef.jpg")
-print(pixelMap[1,1])
-generatePillArt(exp, pixelMap, "20231205.gif")
+colors = smallNeef.getcolors()
+palette = smallNeef.getpalette()
+print(palette[13 * 3])
+print(palette[13 * 3 + 1])
+print(palette[13 * 3 + 2])
+
+
+
+#smallNeef.save("smallneef3.gif")
+#print(pixelMap[0,0])
+#print(pixelMap[1,0])
+#print(pixelMap[2,0])
+#print(pixelMap[3,0])
+#print(pixelMap[4,0])
+#print(pixelMap[5,0])
+#print(pixelMap[6,0]) #13
+#generatePillArt(exp, pixelMap, "20231212.gif")
 
 
 
