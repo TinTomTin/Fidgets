07f96e686e8ddfae3a9110fcce33ac39148485cf
Tomasz Zaniewski <tomasz.zaniewski@gmail.com>
2024-01-18 00:31:06 +0200

    print palette index in pils

--- DrawMe/draw-me-lego.py
+++ DrawMe/draw-me-lego.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 class LegoArtPic:
     def __init__(self, pillSize, pilsX, pilsY):
@@ -17,9 +17,11 @@ def generatePillArt(legoArtPic, inputImage, outFileName):
     colorAdjustedImage = inputImage.convert(mode="P", palette=1, colors=16, dither=3).resize([48, 48])
     inputPixelMap = colorAdjustedImage.load()
     inputPalette = colorAdjustedImage.getpalette()
+    fontOffset = int(legoArtPic.pillSize / 2)
 
     image = Image.new("RGB", (legoArtPic.pixelLength(), legoArtPic.pixelHeight()), "#000012")
     draw = ImageDraw.Draw(image)
+    draw.font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", size=30)
     for x in range(legoArtPic.pilsX):
         for y in range(legoArtPic.pilsY):
             px = x * legoArtPic.pillSize
@@ -29,6 +31,7 @@ def generatePillArt(legoArtPic, inputImage, outFileName):
             outlineColor = (fillColor[0] + 25, fillColor[1] + 25, fillColor[2] + 25)
             points = [(px, py), (px + legoArtPic.pillSize, py + legoArtPic.pillSize)]
             draw.ellipse(points, fill=fillColor, width=3, outline=outlineColor)
+            draw.text((px + fontOffset, py + fontOffset), str(int(paletteIndex / 3)),fill=(200, 200, 200), anchor="mm")
     image.save(outFileName)
     image.show()
 
@@ -41,11 +44,11 @@ inputImage =  Image.open("Boerneef.jpg")
 #rgbImage = colorAdjust.convert(mode="RGB", colors=16, palette=1).resize([48,48])
 #print('Colors in RGB mode image: {cols}'.format(cols=len(rgbImage.getcolors())))
 
-#generatePillArt(exp, inputImage, "20231218.gif")
+generatePillArt(exp, inputImage, "20240117.gif")
 
 
-#TODO: print col number in each pill
 #TODO: split into 9 (3 x 3) tiles
 #TODO: load custom palette
 #TODO: source image as parameter
+#TODO: share using streamlit
 
