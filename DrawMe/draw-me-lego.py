dd8b7f17b2d4f96bace346f25534100bf54b2e62
Tomasz Zaniewski <tomasz.zaniewski@gmail.com>
2024-01-24 00:20:23 +0200

    generates messy legend

--- DrawMe/draw-me-lego.py
+++ DrawMe/draw-me-lego.py
@@ -12,7 +12,25 @@ class LegoArtPic:
     def pixelHeight(self):
         return self.pilsY * self.pillSize
 
+def generateColorLegend(inputImage, outFileName):
+    colorsUsed = inputImage.getcolors()
+    palette = inputImage.getpalette()
+    yStep = 45
+    legendPillSize = 30
+    legendImage = Image.new("RGB", (500, len(colorsUsed) * yStep), "#800000")
+    legendDraw = ImageDraw.Draw(legendImage)
+    legendDraw.font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", size=30)
+    for colNumber in colorsUsed:
+        legendLabel = 'x {cnt}'.format(cnt = colNumber[0])
+        paletteIndex = colNumber[1] * 3
+        fillColor = (palette[paletteIndex], palette[paletteIndex + 1], palette[paletteIndex +2])
+        points = [(30, colNumber[1] * yStep), (60, (colNumber[1] * yStep) + 30)]
+        legendDraw.ellipse(points, fill=fillColor, width=3)
+        legendDraw.text((30, colNumber[1] * yStep), str(colNumber[1]), fill='white')
+        legendDraw.text((70, colNumber[1] * yStep), legendLabel, fill='white')
+    legendImage.save('{fn}-legend.gif'.format(fn=outFileName))
     
+
 def generatePillArt(legoArtPic, inputImage, outFileName):
     colorAdjustedImage = inputImage.convert(mode="P", palette=1, colors=16, dither=3).resize([48, 48])
     inputPixelMap = colorAdjustedImage.load()
@@ -33,7 +51,7 @@ def generatePillArt(legoArtPic, inputImage, outFileName):
             draw.ellipse(points, fill=fillColor, width=3, outline=outlineColor)
             draw.text((px + fontOffset, py + fontOffset), str(int(paletteIndex / 3)),fill=(200, 200, 200), anchor="mm")
     image.save(outFileName)
-    image.show()
+    generateColorLegend(colorAdjustedImage, outFileName)
 
 
 exp = LegoArtPic(50, 48, 48)
@@ -44,11 +62,12 @@ inputImage =  Image.open("Boerneef.jpg")
 #rgbImage = colorAdjust.convert(mode="RGB", colors=16, palette=1).resize([48,48])
 #print('Colors in RGB mode image: {cols}'.format(cols=len(rgbImage.getcolors())))
 
-generatePillArt(exp, inputImage, "20240117.gif")
+generatePillArt(exp, inputImage, "20240123.gif")
 
 
 #TODO: split into 9 (3 x 3) tiles
 #TODO: load custom palette
 #TODO: source image as parameter
+#TODO: generate image with pill legend
 #TODO: share using streamlit
 
